aff0b629478d94b7853fcae3be1904b908e14856
vladiuse <vladiuse@gmail.com>
2023-08-25 21:46:41 +0300

    add readme

--- validators/validators.py
+++ validators/validators.py
@@ -43,7 +43,7 @@ class RegExValidator:
 
     def __call__(self, value):
         if not re.match(self.reg_ex, value):
-            raise ValidationError(f'Значение {value} не подходит под шаблон {self.reg_ex}')
+            raise ValidationError(f'{self.error_text}: Значение "{value}" не подходит под шаблон {self.reg_ex}')
 
 
 class NameRegExValidator(RegExValidator):
